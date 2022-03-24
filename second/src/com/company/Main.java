cf06dd41de6c06f6d4d702903ba3ac4b4d727164
zahidsgn <zahide.dusgun@gmail.com>
2022-03-25 01:14:40 +0300

    Asal Sayı Bulma

---
+++ second/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        int sayi = 111;
+        boolean deger=true;
+        for (int i=2; i<sayi; i++) {
+
+        if(sayi % i == 0){
+            deger=false;
+
+        }  }
+       if(deger){
+           System.out.println("Sayi Asal");
+       }
+       else{
+           System.out.println("Asal Değil");
+       }
+
+        }
+    }
+
+
+
+
+
